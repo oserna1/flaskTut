b509886b74d1e8d5e0ff38a79ea7dbe388fc1c1a
oserna1 <oserna1994@gmail.com>
2020-05-21 21:45:23 -0500

    removed hello.py, added setup.cfg, ammended .gitignore

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='flaskr',
-    version='1.0.0',
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        'flask',
-    ],
-)
+setup()
