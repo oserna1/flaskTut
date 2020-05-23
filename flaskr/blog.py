3b6e8dd34aabd581660b57524d9c8d74ef333a82
oserna1 <oserna1994@gmail.com>
2020-05-23 00:55:06 -0500

    added liked property to blog posts

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,7 +12,7 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, liked, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
@@ -34,9 +34,9 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO post (title, body, liked, author_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (title, body, 0, g.user['id'])
             )
             db.commit()
             return redirect(url_for('blog.index'))
@@ -45,7 +45,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, liked, created, author_id, username'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -65,6 +65,32 @@ def view(id):
     post = get_post(id)
 
     return render_template('blog/view.html', post=post)
+
+@bp.route('/<int:id>/like', methods=('GET','POST'))
+@login_required
+def like(id):
+    post = get_post(id)
+    print(post)
+
+    # liked is 4 property of post, using index of 3
+    # no boolean in sqlite, using 0 and 1
+    if post[3]== 1:
+        db = get_db()
+        db.execute(
+            'UPDATE post SET liked = ?'
+            ' WHERE id = ?',
+            (0, id)
+        )
+        db.commit()
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE post SET liked = ?'
+            ' WHERE id = ?',
+            (1, id)
+        )
+        db.commit()
+    return redirect(url_for('blog.index'))
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
